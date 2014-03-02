3920b15e3a8298e556d944c8657a3e0be446089f
jeffrey <j3ffreyjohn@gmail.com>
2014-03-02 11:38:33 -0500

    Compute Classification Accuracy

--- code/dataUtil.py
+++ code/dataUtil.py
@@ -4,6 +4,17 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
+# Return class name for label
+def getClass(label):
+	dict = {'1': 'Buildings', '2': 'Cars', '3': 'Faces', '4': 'Flowers', '5':'Shoes'}
+	return dict[label]
+
+# Return label for class name
+def getLabel(className):
+	dict = {'Buildings':'1','Cars':'2','Faces':'3','Flowers':'4','Shoes':'5'}
+	return dict[className]
+
+# Parse all data and return as pandas data frames
 def getData(trainFile,trainLabelsFile,validationFile,validationLabelsFile):
 	# Load all data into dictionary
 	trainData = open(trainFile,'r')
@@ -33,11 +44,13 @@ def getData(trainFile,trainLabelsFile,validationFile,validationLabelsFile):
 	dataNP = np.concatenate([[b[1]] for b in data])
 	df = pd.DataFrame(dataNP)
 	df['is_train'] = [b[2] for b in data]
-	df['class'] = [b[3] for b in data]
+	target = np.array([int(b[3])-1 for b in data])
+	target_names = np.array(['Buildings','Cars','Faces','Flowers','Shoes'])
+	df['class'] = pd.Categorical(target,target_names)
 	df.head()
 
 	trainData.close()
 	trainLabels.close()
 	validationData.close()
 	validationLabels.close()
-	return [df,data]
+	return [df,data,target_names]
--- code/rfClassifier.py
+++ code/rfClassifier.py
@@ -9,11 +9,15 @@ trainFile = '../data/Train/feature_vectors.txt'
 trainLabelsFile = '../data/Train/labels.txt'
 validationFile = '../data/Validation/feature_vectors.txt'
 validationLabelsFile = '../data/Validation/trueLabels.txt'
+predictionsFile = '../data/preds.txt'
 
 # Obtain data frame
 dataFrames = DU.getData(trainFile,trainLabelsFile,validationFile,validationLabelsFile)
 df = dataFrames[0]
 data = dataFrames[1]
+target_names = dataFrames[2]
+trainData = [el for el in data if el[2]==1]
+testData = [el for el in data if el[2]==0]
 
 # Obtain train and test data
 train, test = df[df['is_train']==1], df[df['is_train']==0]
@@ -23,8 +27,22 @@ features = df.columns[:-2]
 clf = RandomForestClassifier(n_jobs=2)
 y, _ = pd.factorize(train['class'])
 clf.fit(train[features],y)
-preds = clf.predict(test[features]) 
+preds = target_names[clf.predict(test[features])]
+pd.crosstab(test['class'], preds, rownames=['actual'], colnames=['preds'])
 
 # Output predictions
+predOut = open(predictionsFile,'w')
+for i in range(len(test)):
+	predOut.write(testData[i][0]+' '+DU.getLabel(preds[i])+'\n')
+predOut.close()
 
 # Compute Classification Accuracy
+total=0
+correct = 0
+for i in range(len(preds)):
+	if DU.getLabel(preds[i])==testData[i][3]:
+		correct +=1
+	total +=1
+
+print 'Classification Accuracy : ', (correct*100.0)/total
+	
